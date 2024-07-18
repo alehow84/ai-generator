f5a858b959f8391477eb40d7b08cf399ef37a70e
Alex Howlett <alex.howlett@live.co.uk>
2024-07-18 17:59:06 +0100

    added form handling and Typewriter plugin for form response

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
+let quoteFormEvent = document.querySelector("#quote-form");
 
+function generateQuote(event) {
+  event.preventDefault();
+
+  new Typewriter("#quote", {
+    strings:
+      "The early bird catches the worm, and is the first to get indigestion.",
+    autoStart: true,
+    delay: 5,
+    cursor: "",
+  });
+}
+
+quoteFormEvent.addEventListener("submit", generateQuote);
